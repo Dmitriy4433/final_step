87488860d72c7e538a80496cd15b2888e021ba7f
dmitriy <ponomarenko5015@ukr.net>
2025-09-28 07:42:06 +0000

    deploy v4.0

--- EKS/argo-helm.tf
+++ EKS/argo-helm.tf
@@ -1,51 +1,121 @@
+# locals {
+#   cluster_name = var.name
+#   argocd_host  = "argocd.${local.cluster_name}.${var.zone_name}"
+# }
+
+# resource "helm_release" "argocd" {
+#   name       = "argocd"
+#   repository = "https://argoproj.github.io/argo-helm"
+#   chart      = "argo-cd"
+#   version    = "8.5.6"
+
+#   namespace        = "argocd"
+#   create_namespace = true
+#   wait             = true
+#   timeout          = 900
+
+#   # Використовуємо правильну структуру для ArgoCD v8.5.6
+#   set {
+#     name  = "server.insecure"
+#     # value = "true"
+#   }
+  
+#   set {
+#     name  = "server.service.type"
+#     value = "ClusterIP"
+#   }
+
+#   # Ingress налаштування - правильна структура для v8.5.6
+#   set {
+#     name  = "server.ingress.enabled"
+#     value = "false"
+#   }
+  
+#   set {
+#     name  = "server.ingress.ingressClassName"
+#     value = "nginx"
+#   }
+  
+#   set_list {
+#     name  = "server.ingress.hosts"
+#     value = [local.argocd_host]
+#   }
+
+#   # Анотації
+#   set {
+#     name  = "server.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/ssl-redirect"
+#     value = "false"
+#   }
+  
+#   set {
+#     name  = "server.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/backend-protocol"
+#     value = "HTTP"
+#   }
+  
+#   set {
+#     name  = "server.ingress.annotations.external-dns\\.alpha\\.kubernetes\\.io/hostname"
+#     value = local.argocd_host
+#   }
+
+#   # CRDs
+#   set {
+#     name  = "crds.install"
+#     value = "true"
+#   }
+
+#   depends_on = [
+#     helm_release.nginx_ingress
+#   ]
+# }
+
+# output "argocd_hostname" {
+#   value = local.argocd_host
+# }
 locals {
-  # DNS для ArgoCD: argocd.<cluster-name>.<zone_name>
-  argocd_host = "argocd.${var.name}.${var.zone_name}"
+  cluster_name = var.name
+  argocd_host  = "argocd.${local.cluster_name}.${var.zone_name}"
 }
 
 resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-
+  version    = "8.5.6"
 
   namespace        = "argocd"
   create_namespace = true
+  wait             = true
+  timeout          = 900
 
-  # TLS термінація на NLB, тому сервер слухає HTTP всередині кластера
-  set {
-    name  = "server.insecure"
-    value = "true"
-  }
+  values = [yamlencode({
+    # важливо: ArgoCD повинен знати свій зовнішній URL і що він HTTP
+    configs = {
+      cm = {
+        url = "http://${local.argocd_host}"
+      }
+      params = {
+        "server.insecure" = true
+      }
+    }
 
-  # Тип сервісу для argocd-server
-  set {
-    name  = "server.service.type"
-    value = "ClusterIP"
-  }
+    server = {
+      service = { type = "ClusterIP" }
+      ingress = {
+        enabled          = true
+        ingressClassName = "nginx"
+        hosts            = [local.argocd_host]
+        annotations = {
+          "nginx.ingress.kubernetes.io/ssl-redirect"     = "false"
+          "nginx.ingress.kubernetes.io/backend-protocol" = "HTTP"
+          "external-dns.alpha.kubernetes.io/hostname"    = local.argocd_host
+        }
+      }
+    }
+  })]
 
-  # Ingress через nginx + наш DNS-хост
-  set {
-    name  = "server.ingress.enabled"
-    value = "true"
-  }
-  set {
-    name  = "server.ingress.ingressClassName"
-    value = "nginx"
-  }
-  set {
-    name  = "server.ingress.hosts[0]"
-    value = local.argocd_host
-  }
+  depends_on = [helm_release.nginx_ingress]
+}
 
-  # Встановити CRD (на випадок, якщо не включені за замовчуванням)
-  set {
-    name  = "crds.install"
-    value = "true"
-  }
-
-  depends_on = [
-    aws_eks_node_group.danit-amd,
-    helm_release.nginx_ingress,
-  ]
+output "argocd_hostname" {
+  value = local.argocd_host
 }
---
+++ EKS/argocd-ingress.tf
@@ -0,0 +1,36 @@
+# Створюємо власний ingress для ArgoCD
+resource "kubernetes_ingress_v1" "argocd_custom" {
+  metadata {
+    name      = "argocd-custom"
+    namespace = "argocd"
+    annotations = {
+      "nginx.ingress.kubernetes.io/ssl-redirect"     = "false"
+      "nginx.ingress.kubernetes.io/backend-protocol" = "HTTP"
+      "external-dns.alpha.kubernetes.io/hostname"    = "argocd.${var.name}.${var.zone_name}"
+    }
+  }
+
+  spec {
+    ingress_class_name = "nginx"
+    
+    rule {
+      host = "argocd.${var.name}.${var.zone_name}"
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "argocd-server"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  
+  depends_on = [helm_release.argocd]
+}
