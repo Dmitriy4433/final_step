aa1e1f51075507df06d2c239b6c720ec434bae80
dmitriy <ponomarenko5015@ukr.net>
2025-09-30 21:17:42 +0000

    add to v5.3

--- EKS/argocd-ingress.tf
+++ EKS/argocd-ingress.tf
@@ -1,18 +1,54 @@
-# Створюємо власний ingress для ArgoCD
+# # Створюємо власний ingress для ArgoCD
+# resource "kubernetes_ingress_v1" "argocd_custom" {
+#   metadata {
+#     name      = "argocd-custom"
+#     namespace = "argocd"
+#     annotations = {
+#       "nginx.ingress.kubernetes.io/ssl-redirect"     = "false"
+#       "nginx.ingress.kubernetes.io/backend-protocol" = "HTTP"
+#       "external-dns.alpha.kubernetes.io/hostname"    = "argocd.${var.name}.${var.zone_name}"
+#     }
+#   }
+
+#   spec {
+#     ingress_class_name = "nginx"
+    
+#     rule {
+#       host = "argocd.${var.name}.${var.zone_name}"
+#       http {
+#         path {
+#           path      = "/"
+#           path_type = "Prefix"
+#           backend {
+#             service {
+#               name = "argocd-server"
+#               port {
+#                 number = 80
+#               }
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+  
+#   depends_on = [helm_release.argocd]
+# }
 resource "kubernetes_ingress_v1" "argocd_custom" {
   metadata {
     name      = "argocd-custom"
     namespace = "argocd"
     annotations = {
-      "nginx.ingress.kubernetes.io/ssl-redirect"     = "false"
-      "nginx.ingress.kubernetes.io/backend-protocol" = "HTTP"
-      "external-dns.alpha.kubernetes.io/hostname"    = "argocd.${var.name}.${var.zone_name}"
+      "nginx.ingress.kubernetes.io/ssl-redirect"       = "true"
+      "nginx.ingress.kubernetes.io/force-ssl-redirect" = "true"
+      "nginx.ingress.kubernetes.io/backend-protocol"   = "HTTP"
+      "external-dns.alpha.kubernetes.io/hostname"      = "argocd.${var.name}.${var.zone_name}"
     }
   }
 
   spec {
     ingress_class_name = "nginx"
-    
+
     rule {
       host = "argocd.${var.name}.${var.zone_name}"
       http {
@@ -22,15 +58,13 @@ resource "kubernetes_ingress_v1" "argocd_custom" {
           backend {
             service {
               name = "argocd-server"
-              port {
-                number = 80
-              }
+              port { number = 80 }
             }
           }
         }
       }
     }
   }
-  
+
   depends_on = [helm_release.argocd]
 }
